ceabb82abfb56abf54a5fc4bb5ec18589c1138ef
mhsjlw <mhsjlw@aol.com>
2015-07-25 09:18:38 -0400

    1.0.1

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+exports.contribute = function(req, res){
+  res.render('contribute', { title: 'Contribute - Blend exchange' })
+};
+
+exports.about = function(req, res){
+  res.render('about', { title: 'About - Blend exchange' })
 };
